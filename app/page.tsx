12f3611d0f28bbe2ac2fae5b593c63e296689f0f
RenanSui <renanddtao@gmail.com>
2023-03-12 11:04:45 -0300

    first commit, landing page almost done

--- app/page.tsx
+++ app/page.tsx
@@ -1,91 +1,77 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from './page.module.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSnowflake } from '@fortawesome/free-solid-svg-icons';
+import { faGithub, faLinkedinIn } from '@fortawesome/free-brands-svg-icons';
+import { Space_Grotesk } from '@next/font/google';
 
-const inter = Inter({ subsets: ['latin'] })
+const spaceGrotesk = Space_Grotesk({
+  weight: ['300', '400', '500', '600', '700'],
+  preload: true,
+  subsets: ['latin'],
+});
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
+    <>
+      <header
+        className={`flex w-full border-b border-b-[#1E2435] ${spaceGrotesk.className}`}
+      >
+        <div className="flex basis-1/3 items-center border-r border-r-[#1E2435] py-12 px-10">
+          <a href="/" className="">
+            <FontAwesomeIcon icon={faSnowflake} className="h-8 w-8 text-xs" />
+          </a>
+        </div>
+
+        <div className="flex basis-1/3 justify-center gap-24 border-r border-r-[#1E2435] py-12 px-6 text-xs font-medium tracking-widest text-[#747083]">
+          <a href="" className="hover:text-[#9592a2]">
+            ABOUT
+          </a>
+          <a href="" className="hover:text-[#9592a2]">
+            WORK
+          </a>
+        </div>
+
+        <div className="flex flex-grow justify-center gap-12 border-r border-r-[#1E2435] py-12 px-6">
           <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            href="https://www.youtube.com/watch?v=nG2IyH43xMU"
             target="_blank"
-            rel="noopener noreferrer"
+            rel="noreferrer"
           >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
+            <FontAwesomeIcon
+              icon={faLinkedinIn}
+              className="h-4 w-4 text-xs text-[#777A80]"
+            />
+          </a>
+          <a
+            href="https://www.youtube.com/watch?v=nG2IyH43xMU"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FontAwesomeIcon
+              icon={faGithub}
+              className="h-4 w-4 text-xs text-[#777A80]"
             />
           </a>
         </div>
-      </div>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-        <div className={styles.thirteen}>
-          <Image src="/thirteen.svg" alt="13" width={40} height={31} priority />
+        <div className="flex flex-grow justify-center py-12 px-6">
+          <a href="" className="text-xs tracking-widest text-[#9d84ff]">
+            GET IN TOUCH
+          </a>
         </div>
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+      </header>
+      <main className="relative flex-grow">
+        <article className="absolute left-0 top-0 right-0 bottom-0 grid h-full grid-cols-[2fr_1fr] grid-rows-[2fr_1fr]">
+          <div className="h-full bg-[url('https://images.unsplash.com/photo-1440404653325-ab127d49abc1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80')]"></div>
+          <div className="h-full border-b border-[#1E2435]">description</div>
+          <div className="h-full">title</div>
+          <div className="flex h-full items-center justify-center border-l border-[#1E2435]">
+            <div className='h-full w-1/2 cursor-pointer border-r border-[#1E2435] bg-[url("/SVG/arrow-left.svg")] bg-[length:51px] bg-center bg-no-repeat transition-all duration-300 hover:bg-[#1e243529] hover:bg-[40%_50%]'></div>
+            <div className='h-full w-1/2 cursor-pointer bg-[url("/SVG/arrow-right.svg")] bg-[length:51px] bg-center bg-no-repeat transition-all duration-300 hover:bg-[#1e243529] hover:bg-[60%_50%]'></div>
+          </div>
+        </article>
+        {/* 'http://source.unsplash.com/random/1200x600' */}
+      </main>
+    </>
+  );
 }
+('rgba(30, 36, 53, 0.16)');
