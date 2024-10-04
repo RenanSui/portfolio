4bea7677ff84fe80188366f9369fc32f7794a997
RenanSui <renanddtao@gmail.com>
2024-10-04 16:15:26 -0300

    feat: add metadata

---
+++ src/config/site.ts
@@ -0,0 +1,16 @@
+export const locales = ['en', 'pt-br']
+
+const links = {
+  x: 'https://twitter.com/adsjksui',
+  github: 'https://github.com/renansui/portfolio',
+  githubAccount: 'https://github.com/renansui',
+  discord: 'https://discord.com/users/adsk14',
+}
+
+export const siteConfig = {
+  name: 'Portfolio',
+  description: 'Personal Portfolio Website',
+  url: 'https://renansui.vercel.app',
+  ogImage: 'https://renansui.vercel.app/opengraph-image.png',
+  links,
+}
