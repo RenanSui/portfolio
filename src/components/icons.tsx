037379105232db425c0101d0e65018a7f2b37697
RenanSui <renanddtao@gmail.com>
2024-10-06 00:00:43 -0300

    feat: extend icons

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -21,6 +21,15 @@ export const Icons = {
       />
     </svg>
   ),
+  externalLink: (props: IconProps) => (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" {...props}>
+      <path
+        fillRule="evenodd"
+        d="M5.22 14.78a.75.75 0 001.06 0l7.22-7.22v5.69a.75.75 0 001.5 0v-7.5a.75.75 0 00-.75-.75h-7.5a.75.75 0 000 1.5h5.69l-7.22 7.22a.75.75 0 000 1.06z"
+        clipRule="evenodd"
+      ></path>
+    </svg>
+  ),
   github: (props: IconProps) => (
     <svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
       <path
