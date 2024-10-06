a43ac24cba5425d9acefcdfa90a1a6dd16f4f933
RenanSui <renanddtao@gmail.com>
2024-10-06 15:44:42 -0300

    feat: create not found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,15 @@
+import { ErrorCard } from '@/components/error-card'
+import { Shell } from '@/components/shells/shell'
+
+export default function PageNotFound() {
+  return (
+    <Shell variant="centered" className="mx-auto flex h-screen max-w-md items-center justify-center">
+      <ErrorCard
+        title="Page not found"
+        description="The page you are looking for does not exist"
+        retryLink="/"
+        retryLinkText="Go to Home"
+      />
+    </Shell>
+  )
+}
