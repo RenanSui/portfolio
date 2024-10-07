ea5d2b934a6953199b11b61a0066f642e9cfa0c1
RenanSui <renanddtao@gmail.com>
2024-10-06 21:30:00 -0300

    feat: create intl server actions

---
+++ src/lib/server/intl.ts
@@ -0,0 +1,22 @@
+import { getTranslations } from 'next-intl/server'
+
+export const getMessage = async (locale: string) => {
+  const intl = await getTranslations({ locale, namespace: 'HomePage' })
+
+  return {
+    career: intl('career') ?? '',
+    resumeButton: intl('resumeButton') ?? '',
+    about: {
+      title: intl('sections.about.title') ?? '',
+      description: intl('sections.about.description') ?? '',
+    },
+    works: {
+      title: intl('sections.works.title') ?? '',
+      description: intl('sections.works.description') ?? '',
+    },
+    projects: {
+      title: intl('sections.projects.title') ?? '',
+      description: intl('sections.projects.description') ?? '',
+    },
+  }
+}
