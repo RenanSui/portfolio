8d2c459748d041e238360f52796b1e9aec47a7ca
RenanSui <renanddtao@gmail.com>
2024-10-12 15:02:36 -0300

    feat: extend work item with grouped variant

--- src/stories/work-item.stories.tsx
+++ src/stories/work-item.stories.tsx
@@ -1,8 +1,9 @@
+import { Section } from '@/components/shells/section'
 import { WorkItem } from '@/components/work-item'
 import { type Meta, type StoryObj } from '@storybook/react'
 
 const meta = {
-  title: 'Components/Work Card',
+  title: 'Components/Work Item',
   component: WorkItem,
   tags: ['autodocs'],
   parameters: { layout: 'centered' },
@@ -18,3 +19,17 @@ export default meta
 type Story = StoryObj<typeof meta>
 
 export const Default: Story = {}
+
+export const Grouped: Story = {
+  render: (args) => {
+    return (
+      <Section title="Projects">
+        <div>
+          {Array.from({ length: 4 }).map((_, index) => (
+            <WorkItem key={index} {...args} />
+          ))}
+        </div>
+      </Section>
+    )
+  },
+}
