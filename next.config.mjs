a43e28491bb68a758da3286e62abf705b42ea3ed
RenanSui <renanddtao@gmail.com>
2024-10-12 16:37:55 -0300

    feat: add cache control to next config

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,20 @@ import createNextIntlPlugin from 'next-intl/plugin'
 const withNextIntl = createNextIntlPlugin()
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=0, must-revalidate', // You can adjust this value
+          },
+        ],
+      },
+    ]
+  },
+}
 
 export default withNextIntl(nextConfig)
